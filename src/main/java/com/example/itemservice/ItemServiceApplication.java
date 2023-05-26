01041da25129be227d9bfacfcf1305e386c97148
moin93 <moinmirmasomi1@outlook.com>
2023-05-26 11:02:34 +0200

    ....

--- src/main/java/com/example/itemservice/ItemServiceApplication.java
+++ src/main/java/com/example/itemservice/ItemServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//create database item_service_db ;
 
 @SpringBootApplication
 public class ItemServiceApplication {
