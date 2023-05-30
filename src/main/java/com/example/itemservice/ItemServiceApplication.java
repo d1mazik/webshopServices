eedd5beb6d93906df4a78df1751e7083c5053311
Mark-Masomi <moinmirmasomi@outlook.com>
2023-05-30 10:11:17 +0200

    ....

--- src/main/java/com/example/itemservice/ItemServiceApplication.java
+++ src/main/java/com/example/itemservice/ItemServiceApplication.java
@@ -3,7 +3,10 @@ package com.example.itemservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //create database item_service_db ;
-
+// put this in your mysql workbench and run it before you run the application!
+/*------------------------------------------------------------
+create database item_service_db;
+-------------------------------------------------------------- */
 @SpringBootApplication
 public class ItemServiceApplication {
 
